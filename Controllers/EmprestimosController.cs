90cab00c733003d486278a687237f1320e741c52
anacorrea791 <correaanaclara898@gmail.com>
2026-01-30 16:04:12 -0300

    Adiciona CRUD completo para Categorias, Empréstimos e mais
    Inclui controllers e views scaffold para Categorias, Empréstimos, Renovação e Usuários, permitindo operações completas de CRUD. Atualiza o menu de navegação para acesso rápido a essas entidades. As views utilizam helpers do ASP.NET Core MVC para formulários, tabelas e validação, seguindo o padrão scaffold para facilitar manutenção e expansão do sistema.

---
+++ Controllers/EmprestimosController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sistemas_de_emprestimos_de_livro.Data;
+using Sistemas_de_emprestimos_de_livro.Models;
+
+namespace Sistemas_de_emprestimos_de_livro.Controllers
+{
+    public class EmprestimosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmprestimosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Emprestimos
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Emprestimo.Include(e => e.Livro).Include(e => e.Usuario);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Emprestimos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var emprestimo = await _context.Emprestimo
+                .Include(e => e.Livro)
+                .Include(e => e.Usuario)
+                .FirstOrDefaultAsync(m => m.EmprestimoId == id);
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            return View(emprestimo);
+        }
+
+        // GET: Emprestimos/Create
+        public IActionResult Create()
+        {
+            ViewData["LivroId"] = new SelectList(_context.Livro, "LivroId", "LivroId");
+            ViewData["UsuarioId"] = new SelectList(_context.Usuario, "UsuarioId", "UsuarioId");
+            return View();
+        }
+
+        // POST: Emprestimos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EmprestimoId,DataEmprestimo,DataDevolucaoPrevista,QuantidadeRenovacoes,Status,LivroId,UsuarioId")] Emprestimo emprestimo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(emprestimo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LivroId"] = new SelectList(_context.Livro, "LivroId", "LivroId", emprestimo.LivroId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuario, "UsuarioId", "UsuarioId", emprestimo.UsuarioId);
+            return View(emprestimo);
+        }
+
+        // GET: Emprestimos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var emprestimo = await _context.Emprestimo.FindAsync(id);
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+            ViewData["LivroId"] = new SelectList(_context.Livro, "LivroId", "LivroId", emprestimo.LivroId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuario, "UsuarioId", "UsuarioId", emprestimo.UsuarioId);
+            return View(emprestimo);
+        }
+
+        // POST: Emprestimos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EmprestimoId,DataEmprestimo,DataDevolucaoPrevista,QuantidadeRenovacoes,Status,LivroId,UsuarioId")] Emprestimo emprestimo)
+        {
+            if (id != emprestimo.EmprestimoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(emprestimo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmprestimoExists(emprestimo.EmprestimoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LivroId"] = new SelectList(_context.Livro, "LivroId", "LivroId", emprestimo.LivroId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuario, "UsuarioId", "UsuarioId", emprestimo.UsuarioId);
+            return View(emprestimo);
+        }
+
+        // GET: Emprestimos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var emprestimo = await _context.Emprestimo
+                .Include(e => e.Livro)
+                .Include(e => e.Usuario)
+                .FirstOrDefaultAsync(m => m.EmprestimoId == id);
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            return View(emprestimo);
+        }
+
+        // POST: Emprestimos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var emprestimo = await _context.Emprestimo.FindAsync(id);
+            if (emprestimo != null)
+            {
+                _context.Emprestimo.Remove(emprestimo);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmprestimoExists(int id)
+        {
+            return _context.Emprestimo.Any(e => e.EmprestimoId == id);
+        }
+    }
+}
---
+++ Controllers/RenovacaosController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sistemas_de_emprestimos_de_livro.Data;
+using Sistemas_de_emprestimos_de_livro.Models;
+
+namespace Sistemas_de_emprestimos_de_livro.Controllers
+{
+    public class RenovacaosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RenovacaosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Renovacaos
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Renovacao.Include(r => r.Emprestimo);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Renovacaos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var renovacao = await _context.Renovacao
+                .Include(r => r.Emprestimo)
+                .FirstOrDefaultAsync(m => m.RenovacaoId == id);
+            if (renovacao == null)
+            {
+                return NotFound();
+            }
+
+            return View(renovacao);
+        }
+
+        // GET: Renovacaos/Create
+        public IActionResult Create()
+        {
+            ViewData["EmprestimoId"] = new SelectList(_context.Emprestimo, "EmprestimoId", "EmprestimoId");
+            return View();
+        }
+
+        // POST: Renovacaos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RenovacaoId,DataRenovacao,NovaDataDevolucao,EmprestimoId")] Renovacao renovacao)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(renovacao);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmprestimoId"] = new SelectList(_context.Emprestimo, "EmprestimoId", "EmprestimoId", renovacao.EmprestimoId);
+            return View(renovacao);
+        }
+
+        // GET: Renovacaos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var renovacao = await _context.Renovacao.FindAsync(id);
+            if (renovacao == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmprestimoId"] = new SelectList(_context.Emprestimo, "EmprestimoId", "EmprestimoId", renovacao.EmprestimoId);
+            return View(renovacao);
+        }
+
+        // POST: Renovacaos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RenovacaoId,DataRenovacao,NovaDataDevolucao,EmprestimoId")] Renovacao renovacao)
+        {
+            if (id != renovacao.RenovacaoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(renovacao);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RenovacaoExists(renovacao.RenovacaoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmprestimoId"] = new SelectList(_context.Emprestimo, "EmprestimoId", "EmprestimoId", renovacao.EmprestimoId);
+            return View(renovacao);
+        }
+
+        // GET: Renovacaos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var renovacao = await _context.Renovacao
+                .Include(r => r.Emprestimo)
+                .FirstOrDefaultAsync(m => m.RenovacaoId == id);
+            if (renovacao == null)
+            {
+                return NotFound();
+            }
+
+            return View(renovacao);
+        }
+
+        // POST: Renovacaos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var renovacao = await _context.Renovacao.FindAsync(id);
+            if (renovacao != null)
+            {
+                _context.Renovacao.Remove(renovacao);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RenovacaoExists(int id)
+        {
+            return _context.Renovacao.Any(e => e.RenovacaoId == id);
+        }
+    }
+}
