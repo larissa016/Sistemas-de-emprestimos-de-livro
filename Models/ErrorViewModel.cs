680f29fcae68f4de0bffe2d7ebd0fedf1509bf5e
45687204865 <45687204865@ECFP790D1350101>
2026-01-30 08:34:37 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistemas_de_emprestimos_de_livro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
